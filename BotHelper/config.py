15cdddd2de6e7030ef541ed37f87cae63b068181
F1ss1mm <artem2005200816@gmail.com>
2024-04-29 12:52:44 +0300

    bd

--- BotHelper/config.py
+++ BotHelper/config.py
@@ -1,8 +1,8 @@
 bot_token: str = "6957298453:AAEhNTVTkrSwNpqVxdvqI4qS0N1-Q6N09_8"
 open_weather_token: str = "bd76c9a148852b4f2b7f80fbd5a9a6fa"
 db_config: dict[str, str] = {
-	'db_name': 'rates_and_news',
-	'user': 'User',
-	'password': 'bebra123321',
-	'host': 'localhost:27017'
+	'db_name': 'dailyhelper',
+	'user': 'USER',
+	'password': 'PASSWORD',
+	'host': 'localhost'
 }
